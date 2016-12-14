2af0d60636a674912ac7e6e0f2d3b99d38612f6f
Bill Brubaker <brubaker.bill@gmail.com>
2016-12-14 17:06:18 -0600

    playing with child processes

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-//Need shelljs to use the omxplayer.
-const shell = require('shelljs');
+const os = require('os');
+const fs = require('fs');
+const spawn = require('child_process').spawn;
 
+const ls = spawn('ls', ['-lh', '/home/pi']);
+const pwd = spawn('pwd', []);
+
+
+ls.stdout.on('data', (data) => {
+	console.log(`stdout: ${data}`);
+});
+
+ls.on('close', (code) => {
+	console.log(`child prcess exited with code ${code}`);
+});
+
+pwd.stdout.on('data', (data) => {
+	console.log(`stdout: ${data}`);
+});
+
+pwd.on('close', (cat) => {
+	console.log(`stdout: ${cat}`);
+});
